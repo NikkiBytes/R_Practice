95bc1363d00c80f603b736e3fb5ab9ada0c64651
NikkiBytes <nicholletteacosta@gmail.com>
2019-01-01 20:51:24 -0500

    added

--- chp1_part2.R
+++ chp1_part2.R
@@ -11,5 +11,6 @@ library(ggplot2)
 ggplot(data = mpg) + 
   geom_point(mapping = aes(x = displ, y = hwy))
 
+
 ggplot(data = mpg) + 
-  geom_smooth(mapping = aes(x = displ, y = hwy))
+  geom_smooth(mapping = aes(x = displ, y = hwy))e
