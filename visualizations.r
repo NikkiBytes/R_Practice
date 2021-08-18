f7592f091afe7fe84bad2b82077f1fee0d6be028
Nichollette Acosta <nicholletteacosta@gmail.com>
2021-08-17 23:47:34 -0400

    tidyverse update

--- visualizations.r
+++ visualizations.r
@@ -2,4 +2,14 @@
 #install.packages("tidyverse")
 library(tidyverse)
 
+mpg
+?mpg
 
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy))
+
+#ggplot(data = <DATA>) + 
+#  <GEOM_FUNCTION>(mapping = aes(<MAPPINGS>))
+
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy, color = class))
